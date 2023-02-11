93111bbad99763de715541d7cb53059a2498e3a8
Daniel Evangelista Pereira <daniel.evangelista93@gmail.com>
2023-02-11 00:00:12 -0300

    build: configurando o projeto e adicionando dependencias

---
+++ src/main/java/com/daniboy/securityapp/SecurityappApplication.java
@@ -0,0 +1,12 @@
+package com.daniboy.securityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityappApplication.class, args);
+	}
+}
