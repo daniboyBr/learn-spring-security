dd066042eec4fc7d1520b7a9306a14a6052450f6
Daniel Evangelista Pereira <daniel.evangelista93@gmail.com>
2023-02-12 11:46:28 -0300

    refactor: add code that return use to your own class

--- src/main/java/com/daniboy/securityapp/config/SecurityConfig.java
+++ src/main/java/com/daniboy/securityapp/config/SecurityConfig.java
@@ -1,21 +1,24 @@
 package com.daniboy.securityapp.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.daniboy.securityapp.config.security.JwtAuthFilter;
+import com.daniboy.securityapp.dao.UserDao;
+
 import lombok.RequiredArgsConstructor;
 
 @EnableWebSecurity
@@ -23,19 +26,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
-
-    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
-        new User(
-            "admin.securityapp@email.com",
-            "password", 
-            Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
-        ),
-        new User(
-            "user.securityapp@email.com",
-            "password", 
-            Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
-        )
-    );
+    private final UserDao userDao;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -44,9 +35,34 @@ public class SecurityConfig {
             .anyRequest()
             .authenticated()
             .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authenticationProvider(authenticationProvider())
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // para retorna password sem ecriptacao 
+        // return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -54,12 +70,7 @@ public class SecurityConfig {
         return new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-                
-                return APPLICATION_USERS
-                    .stream()
-                    .filter(u -> u.getUsername().equals(email))
-                    .findFirst()
-                    .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
+                return userDao.findUserByEmail(email);
             }
         };
     }
