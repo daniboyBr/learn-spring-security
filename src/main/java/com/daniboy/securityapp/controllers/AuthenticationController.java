e86617e8fa4c1309f8559e6fe99f50ee88c40699
Daniel Evangelista Pereira <daniel.evangelista93@gmail.com>
2023-02-12 11:44:05 -0300

    feat: add auth endpoint to authenticate user and return token

---
+++ src/main/java/com/daniboy/securityapp/controllers/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.daniboy.securityapp.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniboy.securityapp.config.security.JwtUtils;
+import com.daniboy.securityapp.dao.UserDao;
+import com.daniboy.securityapp.dto.AuthenticationRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDao userDao;
+    private final JwtUtils jwtUtils;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> authenticate(
+        @RequestBody AuthenticationRequest request
+    ) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        final UserDetails user = userDao.findUserByEmail(request.getEmail());
+        if(user != null) {
+            return ResponseEntity.ok(jwtUtils.generateToken(user));
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Some error has occured");
+    }
+
+}
