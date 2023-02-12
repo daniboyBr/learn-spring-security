8cd755f00e665144bc285321409baa666049389f
Daniel Evangelista Pereira <daniel.evangelista93@gmail.com>
2023-02-12 10:22:31 -0300

    feat: add a implementation off UserDetailsService

--- src/main/java/com/daniboy/securityapp/config/SecurityConfig.java
+++ src/main/java/com/daniboy/securityapp/config/SecurityConfig.java
@@ -1,8 +1,17 @@
 package com.daniboy.securityapp.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -14,6 +23,19 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+
+    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+        new User(
+            "admin.securityapp@email.com",
+            "password", 
+            Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        ),
+        new User(
+            "user.securityapp@email.com",
+            "password", 
+            Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+        )
+    );
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,4 +49,19 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+                
+                return APPLICATION_USERS
+                    .stream()
+                    .filter(u -> u.getUsername().equals(email))
+                    .findFirst()
+                    .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
+            }
+        };
+    }
+ 
 }
